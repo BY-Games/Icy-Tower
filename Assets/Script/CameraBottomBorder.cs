9abf3e6f1b8c7fc2ce8665f8861ad2d2afc46ea5
yosefkahlon <yosefkahlon53@gmail.com>
2023-04-25 14:20:19 +0300

    ss

--- Assets/Script/CameraBottomBorder.cs
+++ Assets/Script/CameraBottomBorder.cs
@@ -28,6 +28,7 @@ public class CameraBottomBorder : MonoBehaviour
         //Destroy(collision.gameObject);
     }
 
+<<<<<<< HEAD
 
     //Destroy the player object when it touches the bottom border of the camera in the camera 
     //quite the game
@@ -51,5 +52,12 @@ public class CameraBottomBorder : MonoBehaviour
             Destroy(collision.gameObject);
             RandomPlatform.numOfPlatforms--;
         }
+=======
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("tag:" + collision.name);
+        Destroy(collision.gameObject);
+        RandomPlatform.numOfPlatforms--;
+>>>>>>> parent of 9826d4f (update)
     }
 }
